a02dd243f94ea17b435e50d4ab11dd10a644573c
Mel <mel.vega15@gmail.com>
2019-04-01 16:52:57 -0600

    add

---
+++ ProyectoUnidad5_MAVA/bin/bos_script_R.R
@@ -0,0 +1,38 @@
+#script for Principal Component Analysis
+# Library
+library(SNPRelate)
+library(tidyverse)
+
+# Transform the VCF file into gds format
+vcf.fn<- "bos_listo.vcf.recode.vcf"
+snpgdsVCF2GDS(vcf.fn, "bos_listo.gds", method="biallelic.only")
+sampleinfo_filename <- c("popinfo.txt")
+
+## open gds file
+genofile <- snpgdsOpen('bos_listo.gds')
+snpset_pruned <- snpgdsLDpruning(genofile, autosome.only=F)
+snpset.id <- unlist(snpset_pruned)
+
+#Run the PCA
+pca <- snpgdsPCA(genofile, num.thread = 2, eigen.cnt = 16, 
+                 snp.id = snpset.id, missing.rate = 0.10, 
+                 maf = 0.05,autosome.only = F)
+#Look at the percent variance explained
+pc.percent <- pca$varprop*100
+head(round(pc.percent, 2))
+
+#Load sample information for plotting purposes
+sampleinfo <- read_tsv(sampleinfo_filename)
+
+#Make a dataframe of PCA
+tab <- data.frame(name = pca$sample.id,
+                  EV1 = pca$eigenvect[,1],  
+                  EV2 = pca$eigenvect[,2],    
+                  stringsAsFactors = FALSE)
+
+#Merge the sampleinfo 
+tab <- merge(tab, sampleinfo)
+
+#Plot a PCA 
+ggplot(data=tab,aes(EV1,EV2)) + geom_jitter(aes(color=population),
+                                            width=0.01,height=0.01) + ylab("Principal component 2") + xlab("Principal component 1")
